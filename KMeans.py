6b8df198e3d9bbaf8815fd4ef120e71a43e249db
Rosalie Jardri <117536832+RosalieJA@users.noreply.github.com>
2024-08-05 11:52:34 +0200

    Commentaires

--- KMeans.py
+++ KMeans.py
@@ -1,3 +1,4 @@
+#INSTALLATION DES BIBLIOTHEQUES
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
@@ -10,11 +11,11 @@ from codecarbon import EmissionsTracker
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# convert each image to 1 dimensional array
+# CONVERSION DE CHAQUE IMAGE EN UN VECTEUR DE DIMENSION 1
 X = x_train.reshape(len(x_train),-1)
 Y = y_train
 
-# normalize the data to 0 - 1
+# NORMALISATION DES DONNEES (0 - 1)
 X = X.astype(float) / 255.
 
 print(X.shape)
@@ -23,43 +24,43 @@ print(X[0].shape)
 n_digits = len(np.unique(y_test))
 print(n_digits)
 
-# Initialize KMeans model
+# INITIALISATION DU MODELE KMeans
 kmeans = MiniBatchKMeans(n_clusters = n_digits)
 
-# Fit the model to the training data
+# AJUSTER LE MODELE AUX DONNEES D'APPRENTISSAGE
 kmeans.fit(X)
 kmeans.labels_
 
 
 def infer_cluster_labels(kmeans, actual_labels):
     """
-    Associates most probable label with each cluster in KMeans model
-    returns: dictionary of clusters assigned to each label
+    Associe l'étiquette la plus probable à chaque grappe dans le modèle KMeans
+    retourne : dictionnaire des grappes assignées à chaque étiquette
     """
 
     inferred_labels = {}
 
     for i in range(kmeans.n_clusters):
 
-        # find index of points in cluster
+        # Trouver l'index des points dans le cluster
         labels = []
         index = np.where(kmeans.labels_ == i)
 
-        # append actual labels for each point in cluster
+        # Ajouter les étiquettes pour chaque point du cluster
         labels.append(actual_labels[index])
 
-        # determine most common label
+        # Déterminer l'étiquette la plus courante
         if len(labels[0]) == 1:
             counts = np.bincount(labels[0])
         else:
             counts = np.bincount(np.squeeze(labels))
 
-        # assign the cluster to a value in the inferred_labels dictionary
+        # assigner le cluster à une valeur dans le dictionnaire inferred_labels
         if np.argmax(counts) in inferred_labels:
-            # append the new number to the existing array at this slot
+            # ajouter le nouveau numéro au tableau existant à ce slot
             inferred_labels[np.argmax(counts)].append(i)
         else:
-            # create a new array in this slot
+            # Créer un nouveau tableau dans ce slot
             inferred_labels[np.argmax(counts)] = [i]
 
         #print(labels)
@@ -69,11 +70,11 @@ def infer_cluster_labels(kmeans, actual_labels):
 
 def infer_data_labels(X_labels, cluster_labels):
     """
-    Determines label for each array, depending on the cluster it has been assigned to.
-    returns: predicted labels for each array
+    Détermine l'étiquette de chaque tableau, en fonction du groupe auquel il a été assigné.
+    retourne : étiquettes prédites pour chaque tableau
     """
 
-    # empty array of len(X)
+    # tableau vide de taille len(X)
     predicted_labels = np.zeros(len(X_labels)).astype(np.uint8)
 
     for i, cluster in enumerate(X_labels):
@@ -84,7 +85,7 @@ def infer_data_labels(X_labels, cluster_labels):
     return predicted_labels
 
 
-# test the infer_cluster_labels() and infer_data_labels() functions
+# Test des fonctions infer_cluster_labels() et infer_data_labels()
 cluster_labels = infer_cluster_labels(kmeans, Y)
 X_clusters = kmeans.predict(X)
 predicted_labels = infer_data_labels(X_clusters, cluster_labels)
@@ -92,7 +93,7 @@ print (predicted_labels[:20])
 print (Y[:20])
 
 
-# Calculate and print metrics
+# Calcul et affichage des métriques
 def calculate_metrics(estimator, data, labels):
     print('Number of Clusters: {}'.format(estimator.n_clusters))
     print('Inertia: {}'.format(estimator.inertia_))
@@ -100,44 +101,44 @@ def calculate_metrics(estimator, data, labels):
 
 clusters = [10, 16, 36, 64, 144, 256]
 
-# test different numbers of clusters
+# Test de différents nombres de clusters
 for n_clusters in clusters:
     tracker = EmissionsTracker()
     tracker.start()
     estimator = MiniBatchKMeans(n_clusters = n_clusters)
     estimator.fit(X)
 
-    # print cluster metrics
+    # Affichage des metrics du cluster
     calculate_metrics(estimator, X, Y)
 
-    # determine predicted labels
+    # Détermination des étiquettes prédites
     cluster_labels = infer_cluster_labels(estimator, Y)
     predicted_Y = infer_data_labels(estimator.labels_, cluster_labels)
 
-    # calculate and print accuracy
+    # Calcul et affichage de la précision (accuracy)
     print('Accuracy: {}\n'.format(metrics.accuracy_score(Y, predicted_Y)))
     emissions = tracker.stop()
     print(f"Emissions : {emissions} kg CO2")
 
-# test kmeans algorithm on testing dataset
+# Test de l'algorithme des kmeans sur l'ensemble de données de test
 X_test = x_test.reshape(len(x_test),-1)
 X_test = X_test.astype(float) / 255.
 
 tracker = EmissionsTracker()
 tracker.start()
-# initialize and fit KMeans algorithm on training data
+# Initialisation et adaptation de l'algorithme KMeans aux données d'apprentissage
 kmeans = MiniBatchKMeans(n_clusters = 256)
 kmeans.fit(X)
 cluster_labels = infer_cluster_labels(kmeans, Y)
 emissions = tracker.stop()
 print(f"Emissions : {emissions} kg CO2")
     
-# predict labels for testing data
+# Prédiction des étiquettes pour les données de test
 test_clusters = kmeans.predict(X_test)
 print(type(X_test))
 predicted_labels = infer_data_labels(kmeans.predict(X_test), cluster_labels)
 
-# calculate and print accuracy
+# Calcul et affichage de la précision
 print('Accuracy: {}\n'.format(metrics.accuracy_score(y_test, predicted_labels)))
 
 #Test sur nos images
